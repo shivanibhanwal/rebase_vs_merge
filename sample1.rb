043aa069f63f5844724f7eb0e4c08ff620412732
Rohan Daxini <rohan_daxini@yahoo.com>
2015-04-20 16:17:28 +0530

    wip

--- sample1.rb
+++ sample1.rb
@@ -4,5 +4,6 @@ class sample1
     print 'enter your name:'     # print does not terminate with default \n at the end of execution
     name = gets.chomp            # read from stdin
     puts "Hi! #{name}"           # interpolates the string, replaces name with its value
+    puts "Rohan"
   end
 end
