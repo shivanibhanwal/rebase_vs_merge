75a2399bdcfe6c30daf787abaf9a6d6aa696ab04
Shivani <shivani@kiprosh.com>
2015-04-20 16:17:36 +0530

    added names

--- sample1.rb
+++ sample1.rb
@@ -1,10 +1,11 @@
 class sample1
   def check_puts
-    puts 'Hi!'                   # puts the string to stdout
+    puts 'Hi Swati!'                   # puts the string to stdout
     print 'enter your name:'     # print does not terminate with default \n at the end of execution
     name = gets.chomp            # read from stdin
     puts "Hi! #{name}"           # interpolates the string, replaces name with its value
     puts "Rohan"
     puts "Swati"
+    puts "What's Happening Varun! #{name}"           # interpolates the string, replaces name with its value
   end
 end
