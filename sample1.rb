a97038902782c3584d1558764ec9b63c1fb0893a
swatijadhav <swati.jadhav@kiprosh.com>
2015-04-20 16:18:25 +0530

    fixed indentation

--- sample1.rb
+++ sample1.rb
@@ -1,9 +1,9 @@
-class sample1
+class Sample1
   def check_puts
-    puts 'Hi!'                   # puts the string to stdout
-    print 'enter your name:'     # print does not terminate with default \n at the end of execution
-    name = gets.chomp            # read from stdin
-    puts "Hi! #{name}"           # interpolates the string, replaces name with its value
+    puts 'Hi!'               # puts the string to stdout
+    print 'enter your name:' # print does not terminate with default \n at the end of execution
+    name = gets.chomp        # read from stdin
+    puts "Hi! #{name}"       # interpolates the string, replaces name with its value
     puts "Rohan"
     puts "Swati"
   end
